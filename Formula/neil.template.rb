aed5171b966e94ab25e1f2f714e12cb7ae0a0b3c
Michiel Borkent <michielborkent@gmail.com>
2021-09-28 23:15:28 +0200

    Automate brew

--- Formula/neil.template.rb
+++ Formula/neil.template.rb
@@ -2,10 +2,10 @@ class Neil < Formula
   desc ""
   homepage ""
   license ""
-  url "https://github.com/babashka/neil/archive/v0.0.8.zip"
-  sha256 "b63dfdaad89740d9b771ccd59630906a63376ec1b313ec73aafba080cff4dc88"
+  url "https://github.com/babashka/neil/archive/v{{version}}.zip"
+  sha256 "{{sha}}"
 
-  version "0.0.8"
+  version "{{version}}"
 
   depends_on "borkdude/brew/babashka"
 
