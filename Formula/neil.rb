58f70d0ad02cce33e5b3a4e09e7446cace2fdc4c
Michiel Borkent <michielborkent@gmail.com>
2021-09-28 13:06:10 +0200

    Update neil

--- Formula/neil.rb
+++ Formula/neil.rb
@@ -2,10 +2,10 @@ class Neil < Formula
   desc ""
   homepage ""
   license ""
-  url "https://github.com/babashka/neil/archive/cf77896adc2bb0fb379f0042c52b17cf8184c7f1.zip"
-  sha256 "49dbfdaaddfcded38beef51f76bb667bf48a7543796eeff46f73e1e2a37b0472"
+  url "https://github.com/babashka/neil/archive/b270da6bc85532ed21bb23478697938c79e1d8bf.zip"
+  sha256 "0e4744ab9bfc3bff7f42df2420352242ed5a7954619d3c6c802f7c12ac305032"
 
-  version "0.0.1"
+  version "0.0.2"
 
   depends_on "borkdude/brew/babashka" => :build
 
