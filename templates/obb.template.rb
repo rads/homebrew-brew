e01cc17de469bdbab3d4474ddf94096be237e7e7
Michiel Borkent <michielborkent@gmail.com>
2022-03-09 10:27:11 +0100

    Prepare for lazy loaded obb (#1)

--- templates/obb.template.rb
+++ templates/obb.template.rb
@@ -1,17 +1,21 @@
 class Obb{{at}} < Formula
-  desc ""
-  homepage ""
-  license ""
+  desc "Ad-hoc ClojureScript scripting of Mac applications via Apple's Open Scripting Architecture."
+  homepage "https://github.com/babashka/obb"
+  license "EPL-1.0"
   url "https://github.com/babashka/obb/releases/download/v{{version}}/obb.tar.gz"
+  # for testing locally:
+  # url "http://localhost:8090/out/obb.tar.gz"
   sha256 "{{sha}}"
-
   version "{{version}}"
 
+  uses_from_macos "ruby" => :build
+
   def install
-    bin.install "obb"
+    system "./brew_install.sh", prefix
   end
 
   test do
     %x[obb -e '(+ 1 2 3)']
+    %x[obb -e "(require '[obb.repl])"]
   end
 end
