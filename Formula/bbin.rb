7c0bc48fcc48abc686da6463bbb998aaff3ea7e0
Radford Smith <11401+rads@users.noreply.github.com>
2022-09-03 08:29:03 -0700

    Add bbin@0.0.10 (#3)
    * Add bbin@0.0.10

---
+++ Formula/bbin.rb
@@ -0,0 +1,15 @@
+class Bbin < Formula
+  desc ""
+  homepage ""
+  license ""
+  url "https://github.com/babashka/bbin/archive/refs/tags/v0.0.10.zip"
+  sha256 "846d066fa3d45cf9f4a8ae641abfa6b1803f256568074ea678746bcc8464b243"
+
+  version "0.0.10"
+
+  depends_on "borkdude/brew/babashka"
+
+  def install
+    bin.install "bbin"
+  end
+end
